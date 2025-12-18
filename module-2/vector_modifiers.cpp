5f8fb2406a99764c708e0a7de65c9fb6a74351e6
MD Waliullah <waliullah.godev@gmail.com>
2025-12-18 19:06:22 +0600

    replace

--- module-2/vector_modifiers.cpp
+++ module-2/vector_modifiers.cpp
@@ -4,20 +4,22 @@ using namespace std;
 int main()
 {
 
-    vector<int> v = {1, 2, 3, 7, 8};
+    vector<int> v = {1, 2, 3, 7, 8, 2, 3, 2};
     vector<int> v3 = {4, 5, 6};
 
     vector<int> v2;
     v2 = v;
     // v2.insert(v2.begin() + 3, 4);
-    v2.insert(v2.begin() + 3, v3.begin(), v3.end());
+    // v2.insert(v2.begin() + 3, v3.begin(), v3.end());
 
     // for (int i = 0; i < v2.size(); i++)
     // {
     //     cout << v2[i] << " ";
     // }
 
-    for (int x : v2)
+
+    replace(v.begin(), v.end(), 2, 100);
+    for (int x : v)
         cout << x << " ";
 
     return 0;
