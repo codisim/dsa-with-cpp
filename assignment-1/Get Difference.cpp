d4ff98e8a43a4aa98350c6b645bcc8d283e0b879
MD Waliullah <waliullah.godev@gmail.com>
2026-01-01 20:07:52 +0600

    get gifference

---
+++ assignment-1/Get Difference.cpp
@@ -0,0 +1,89 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *next;
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+
+void insert_at_tail(Node *&head, Node *&tail, int val)
+{
+    Node *newnode = new Node(val);
+
+    if (head == NULL)
+    {
+        head = newnode;
+        tail = newnode;
+        return;
+    }
+
+    tail->next = newnode;
+    tail = tail->next;
+}
+
+void sort_link_list(Node *head)
+{
+    for (Node *i = head; i->next != NULL; i = i->next)
+    {
+        for (Node *j = i->next; j != NULL; j = j->next)
+        {
+            if (i->val > j->val)
+            {
+                swap(i->val, j->val);
+            }
+        }
+    }
+}
+
+void get_difference(Node *head)
+{
+
+    if (head == NULL)
+    {
+        cout << 0;
+        return;
+    }
+
+    int firstValue = head->val;
+
+    Node *temp = head;
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+
+    int lastValue = temp->val;
+
+    cout << lastValue - firstValue;
+}
+
+int main()
+{
+
+    Node *head = NULL;
+    Node *tail = NULL;
+
+    int val;
+    while (true)
+    {
+        cin >> val;
+        if (val == -1)
+        {
+            break;
+        }
+
+        insert_at_tail(head, tail, val);
+    }
+
+    sort_link_list(head);
+    get_difference(head);
+
+    return 0;
+}
