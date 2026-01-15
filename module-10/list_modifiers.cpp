c6194b8a0ed70e186769aa31b932b10699a77184
MD Waliullah <waliullah.godev@gmail.com>
2026-01-15 09:55:14 +0600

    modifiers

---
+++ module-10/list_modifiers.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    list<int> l = {1, 2, 3};
+    list<int> l2;
+    // l2 = l;
+    l2.assign(l.begin(), l.end());
+
+    l.push_back(99);
+    l.push_front(100);
+
+    for (int val : l)
+    {
+        cout << val << endl;
+    }
+
+    return 0;
+}
